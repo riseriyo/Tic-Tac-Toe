5b5ac21b212a1723bd7b00509fb631027bee37f2
riseriyo <riseriyo@gmail.com>
2014-07-21 11:41:45 -0700

    tested base case functions, has_won and has_tied; helper functions has_empty_squares for minimax()

--- tictactoe/ai/tests.py
+++ tictactoe/ai/tests.py
@@ -1,3 +1,50 @@
 from django.test import TestCase
+from django.utils import unittest
+#import unittest
 
-# Create your tests here.
+#from .models import Entry, Comment
+#from .forms import CommentForm
+from .views import has_won
+from .views import has_tied
+from .views import has_empty_squares
+
+class MiniMaxTest(unittest.TestCase):
+	'''Tests for minimax function'''
+
+	def setUp(self):
+		win_combos = [	[0, 1, 2], [3, 4, 5], [6, 7, 8],
+						[0, 3, 6], [1, 4, 7], [2, 5, 8],
+						[0, 4, 8], [2, 4, 6]
+					]
+
+	def test_x_has_won(self):
+		board = ['x', 'o', '', 'x', '', '', 'x', 'x', 'o']
+		player = 'x'
+		result = has_won(board, player)
+		self.assertEqual(result, True)
+
+	def test_x_has_not_won(self):
+		board = ['x', 'o', '', '', '', '', 'x', 'x', 'o']
+		player = 'x'
+		result = has_won(board,player)
+		self.assertEqual(result,False)
+
+	def test_has_empty_squares(self):
+		board =  ['', '', '', '', '', '', '', '', '']
+		result = has_empty_squares(board)
+		self.assertEqual(result, True)
+
+	def test_has_one_empty_square(self):
+		board = ['x', 'o', 'x', 'x', 'o', 'o', 'o', 'x', '']
+		result = has_empty_squares(board)
+		self.assertEqual(result, True)
+
+	def test_has_tied(self):
+		board = ['x', 'o', 'x', 'o', 'x', 'x', 'o', 'x', 'o']
+		result = has_tied(board)
+		self.assertEqual(result, True)
+
+	def test_has_not_tied(self):
+		board = ['x', 'o', 'x', 'x', 'o', 'x', 'o', '', '']
+		result = has_tied(board)
+		self.assertEqual(result, False)
--- tictactoe/ai/views.py
+++ tictactoe/ai/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
 
-# Create your views here.
+win_combos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+board = ['' for index in range(9)]
+
+def minimax(board, player):
+	if hasWon(board,player):
+		if player == 'x':
+			return 'X wins!'
+		else:
+			return "O wins!"
+	elif has_tied(board):
+		return "It's a tie!"
+
+	else:
+		pass
+
+def has_won(board, player):
+	combo_list = []
+	frequency_dict = {}
+	board_positions = [ind for ind, val in enumerate(board) if val == player]
+	combo_list = [combo for position in board_positions for combo in win_combos if position in combo]
+	frequency_dict = {str(x): combo_list.count(x) for x in combo_list}
+	for key,val in frequency_dict.items():
+		if val == 3:
+		    return True
+	return False
+
+def has_tied(board):
+	if has_empty_squares(board):
+		return False
+	return True
+
+def has_empty_squares(board):
+	board_positions = [ind for ind,val in enumerate(board) if val == 'x' or val == 'o']
+	if len(board_positions) < 9:
+		return True
+	return False
